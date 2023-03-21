a830a006174614d4d89d40833f43bdfbb520ccac
thyago_silc <thyago_silc@outlook.com>
2023-03-21 17:34:44 -0300

    Realizado ajuste no formato da data e router

--- AppAngular/src/app/app-routing.module.ts
+++ AppAngular/src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TarefasComponent } from './components/tarefas/tarefas.component';
 
-const routes: Routes = [{
-  path:'tarefas', component: TarefasComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: TarefasComponent,
+  },
+  {
+    path: 'tarefas',
+    component: TarefasComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
